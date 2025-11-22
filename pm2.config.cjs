fbfaefe0a82db689cbc36fc1a30918f4c9d4e91b
luciD <sosalnet@yandex.ru>
2025-11-23 00:09:11 +0300

    update pm2 config

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'update-backend',
+            name: 'backend',
             script: 'app.js',
             env: {
                 NODE_ENV: 'production',
